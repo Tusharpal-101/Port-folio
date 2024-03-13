113e4e68758c5f2f109f596e6e031b509e530ee2
Tushar Pal <tpal90068@gmail.com>
2024-03-13 12:46:57 +0530

    first commit

---
+++ src/component/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div
+      name="About"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div>
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+              About
+            </p>
+          </div>
+
+          <p className="text-xl mt-20">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae vel
+            tempore repellendus rem, libero possimus veniam nihil eaque culpa
+            voluptatibus m ollitia numquam dolorum omnis doloremque quisquam
+            modi ab, odio qui unde exercitationem id. Debitis veniam magni
+            numquam modi, enim tempore assumenda laudantium neque possimus
+            molestiae nulla dicta ex quam nobis.
+          </p>
+          <br />
+
+          <p className="text-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugia t
+            maxime odio minus sunt, facilis magni magnam veritatis aut animi asp
+            ernatur deserunt quaerat voluptatum ea quia earum cum voluptas
+            dolorum placeat.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/component/Experience.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import html from '../Assert/html.png'
+import css from '../Assert/css.png'
+import javascript from '../Assert/javascript.png'
+import tailwind from '../Assert/tailwind.png'
+import react from '../Assert/react.png'
+import node from '../Assert/node.png'
+
+const Experience = () => {
+  const text = [
+    {
+      id: 1,
+      src: html,
+      title: 'html',
+      style: 'shadow-orange-500',
+    },
+    {
+      id: 2,
+      src: css,
+      title: 'Css',
+      style: 'shadow-blue-500',
+    },
+    {
+      id: 3,
+      src: react,
+      title: 'react',
+      style: 'shadow-sky-500',
+    },
+    {
+      id: 4,
+      src: javascript,
+      title: 'javascript',
+      style: 'shadow-yellow-500',
+    },
+    {
+      id: 5,
+      src: node,
+      title: 'node',
+      style: 'shadow-green-500',
+    },
+    {
+      id: 6,
+      src: tailwind,
+      title: 'tailwind',
+      style: 'shadow-sky-500',
+    },
+    {
+      id: 7,
+      src: html,
+      title: 'html',
+      style: 'shadow-orange-500',
+    },
+    {
+      id: 8,
+      src: html,
+      title: 'html',
+      style: 'shadow-orange-500',
+    },
+  ]
+
+  return (
+    <div
+      name="Experience"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Experience
+          </p>
+          <p className="py-6">These are the Technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+          {text.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt={title} className="w-20 mx-auto" />
+              <p className="mt-4 ">{title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
